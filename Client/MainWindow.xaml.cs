7d53679af21e307ffadbe9a183904c1c59717959
Malka Schildt <malka_88@outlook.com>
2019-04-29 16:34:42 +0700

    add db

--- Client/MainWindow.xaml.cs
+++ Client/MainWindow.xaml.cs
@@ -49,6 +49,23 @@ namespace Client
             {
                 MessageBox.Show(ex.Message);
             }
+
+            try
+            {
+                string username = "cl" + LoginTb.Text;
+                string password = "cp" + PasswdTb.Password.ToString();
+
+                byte[] un = Encoding.Unicode.GetBytes(username);
+                byte[] pw = Encoding.Unicode.GetBytes(password);
+
+                stream.Write(un, 0, un.Length);
+                stream.Write(pw, 0, pw.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
             SignInGrid.Visibility = Visibility.Hidden;
             MainSpace.Visibility = Visibility.Visible;
         }
@@ -71,7 +88,6 @@ namespace Client
 
                     string message = builder.ToString();
                     string serverCodeResponse = message.Substring(0, 3);
-                    string clientCodeRequest;
 
                     switch (serverCodeResponse)
                     {
@@ -79,13 +95,6 @@ namespace Client
                         case "srp": break;
                         case "sub": break;
                         case "scm": break;
-                    }
-
-                    switch (clientCodeRequest)
-                    {
-                        case "cm": break;
-                        case "cp": break;
-                        case "cl": break;
                     }
                 }
             }
@@ -104,6 +113,21 @@ namespace Client
         private void LoginReg_TextChanged(object sender, TextChangedEventArgs e)
         {
 
+        }
+
+        private void SendMess_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string message = "cm" + Message.Text;
+
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                stream.Write(data, 0, data.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
--- server/MainWindow.xaml.cs
+++ server/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Text;
+using System.Data.SQLite;
 
 namespace server
 {
@@ -78,6 +79,8 @@ namespace server
 
         public void Process(TcpClient tcpClient)
         {
+            int ex = -1;
+            string us = "";
             TcpClient client = tcpClient;
             NetworkStream stream = null;
 
@@ -98,22 +101,67 @@ namespace server
 
                     string message = builder.ToString();
                     string clientCode = message.Substring(0, 2);
-                    string serverCode;
+                    message = message.Substring(3, message.Length);
+
+                    string db_name = "C:\\Users\\Chudo\\source\\repos\\KursaChat\\KursaChat.db";
+                    SQLiteConnection m_dbConnection;
+                    m_dbConnection = new SQLiteConnection("Data Source=" + db_name + ";Version=3;");
+                    m_dbConnection.Open();
+
+                    string response = "";
 
                     switch (clientCode)
                     {
-                        case "cm": break;
-                        case "cp": break;
-                        case "cl": break;
+                        case "cl":
+                        {                           
+                             string sqlCL = "SELECT COUNT(*) FROM Users WHERE Username = '" + message + "'";
+                             SQLiteCommand command = new SQLiteCommand(sqlCL, m_dbConnection);
+                             object reader = command.ExecuteScalar();
+                             int exist = Convert.ToInt32(reader);
+                             us = message;
+                             if (exist == 0)
+                             { 
+                                ex = 0;
+                             }
+                             else
+                             {
+                                ex = 1;
+                             }
+                        }
+                        break;
+
+                        case "cp":
+                            {
+
+                                if (ex == 0)
+                                {
+                                    string add = "INSERT INTO Users(Username, Password) VALUES('" + us + "', '" + message + "')";
+                                    response = "srp";
+                                }
+                                else
+                                {
+                                    string sqlCP = "SELECT COUNT(*) FROM Users WHERE Username = '" + us + "' AND Password = '" + message + "'";
+                                    SQLiteCommand command = new SQLiteCommand(sqlCP, m_dbConnection);
+                                    object reader = command.ExecuteScalar();
+                                    int exist = Convert.ToInt32(reader);
+                                    if(exist == 0)
+                                    {
+                                        response = "swp";
+                                    }
+                                    else
+                                    {
+                                        response = "srp";
+                                    }
+                                }
+                            }
+                            break;
+
+                        case "cm":
+                            string sqlCM = "INSERT INTO GeneralMes(Username, Message) VALUES('" + us + "', '" + message + "')";
+                            break;
                     }
 
-                    switch (serverCode)
-                    {
-                        case "swp": break;
-                        case "srp": break;
-                        case "sub": break;
-                        case "scm": break;
-                    }
+                    m_dbConnection.Close();
 
                     data = Encoding.Unicode.GetBytes(message);
                     stream.Write(data, 0, data.Length);
